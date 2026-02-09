05fa65b9e32444a67c0f70c73cba74b6545fd721
YakVash <yakmivashum123@gmail.com>
2026-02-09 21:50:07 +0530

    Create basic 9x9 Sudoku grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const board = document.getElementById("board");
+
+function createBoard() {
+    board.innerHTML = "";
+    for (let i = 0; i < 81; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        board.appendChild(cell);
+    }
+}
+
+createBoard();
