82320d45d225b8017b926eab5c2d2de6df241b9c
YakVash <yakmivashum123@gmail.com>
2026-02-14 20:39:58 +0530

    Add predefined Sudoku puzzle with locked prefilled cells

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const board = document.getElementById("board");
 let selectedCell = null;
 
+const puzzle = [
+  [5,3,0,0,7,0,0,0,0],
+  [6,0,0,1,9,5,0,0,0],
+  [0,9,8,0,0,0,0,6,0],
+  [8,0,0,0,6,0,0,0,3],
+  [4,0,0,8,0,3,0,0,1],
+  [7,0,0,0,2,0,0,0,6],
+  [0,6,0,0,0,0,2,8,0],
+  [0,0,0,4,1,9,0,0,5],
+  [0,0,0,0,8,0,0,7,9]
+];
+
 function createBoard() {
     board.innerHTML = "";
 
@@ -34,7 +46,8 @@ createBoard();
 document.addEventListener("keydown", function (event) {
 
     // If no cell is selected, do nothing
-    if (!selectedCell) return;
+    if (!selectedCell || selectedCell.classList.contains("prefilled")) return;
+
 
     // If key is between 1 and 9
     if (event.key >= "1" && event.key <= "9") {
