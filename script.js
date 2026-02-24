761e79ade60b4c6351cea8f9078dcea025fe7268
YakVash <yakmivashum123@gmail.com>
2026-02-24 21:57:44 +0530

    Add difficulty selection with multiple puzzles

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let selectedCell = null;
 let seconds = 0;
 let timerInterval = null;
 
+
+
 function startTimer() {
     timerInterval = setInterval(() => {
         seconds++;
@@ -24,17 +26,46 @@ function stopTimer() {
     clearInterval(timerInterval);
 }
 
-const puzzle = [
-  [5,3,0,0,7,0,0,0,0],
-  [6,0,0,1,9,5,0,0,0],
-  [0,9,8,0,0,0,0,6,0],
-  [8,0,0,0,6,0,0,0,3],
-  [4,0,0,8,0,3,0,0,1],
-  [7,0,0,0,2,0,0,0,6],
-  [0,6,0,0,0,0,2,8,0],
-  [0,0,0,4,1,9,0,0,5],
-  [0,0,0,0,8,0,0,7,9]
-];
+const puzzles = {
+    easy: [
+      [5,3,0,0,7,0,0,0,0],
+      [6,0,0,1,9,5,0,0,0],
+      [0,9,8,0,0,0,0,6,0],
+      [8,0,0,0,6,0,0,0,3],
+      [4,0,0,8,0,3,0,0,1],
+      [7,0,0,0,2,0,0,0,6],
+      [0,6,0,0,0,0,2,8,0],
+      [0,0,0,4,1,9,0,0,5],
+      [0,0,0,0,8,0,0,7,9]
+    ],
+
+    medium: [
+      [0,0,0,2,6,0,7,0,1],
+      [6,8,0,0,7,0,0,9,0],
+      [1,9,0,0,0,4,5,0,0],
+      [8,2,0,1,0,0,0,4,0],
+      [0,0,4,6,0,2,9,0,0],
+      [0,5,0,0,0,3,0,2,8],
+      [0,0,9,3,0,0,0,7,4],
+      [0,4,0,0,5,0,0,3,6],
+      [7,0,3,0,1,8,0,0,0]
+    ],
+
+    hard: [
+      [0,0,0,0,0,0,0,1,2],
+      [0,0,0,0,3,5,0,0,0],
+      [0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,3,0,0],
+      [0,0,0,5,0,9,0,0,0],
+      [0,0,7,0,0,0,0,0,0],
+      [0,0,0,0,0,0,6,0,0],
+      [0,0,0,1,2,0,0,0,0],
+      [8,4,0,0,0,0,0,0,0]
+    ]
+};
+
+let currentDifficulty = "easy";
+let puzzle = puzzles[currentDifficulty];
 
 function createBoard() {
     board.innerHTML = "";
@@ -116,6 +147,14 @@ document.addEventListener("keydown", function (event) {
         }
     }  
 
+});
+
+document.getElementById("difficulty").addEventListener("change", function(event) {
+
+    currentDifficulty = event.target.value;
+    puzzle = puzzles[currentDifficulty];
+
+    resetGame();
 });
 
 //create validation Function
